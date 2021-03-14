19c959d261ad2c3ea1cf0893457d14618fbe6724
icefirewater <50715640+icefirewater@users.noreply.github.com>
2021-03-14 23:56:25 +0530

    MyFirstScript
    Functions - Awake, Start, Update & FixedUpdate

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // Awake is called before Start Function
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    // FixedUpdate is called every 0.02 seconds
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+}
